f070ea29b673d68ad5d0c53be3b994ac855362fa
juno <sj202117@gmail.com>
2025-05-27 00:47:06 +0900

    리팩토링 :: 컴포넌트 조건 렌더링 구조 개선

--- src/component/Playback.tsx
+++ src/component/Playback.tsx
@@ -6,42 +6,48 @@ interface PlaybackProps {
 }
 
 export function Playback({ recordedUrl }: PlaybackProps) {
-  return (
-    <div className="flex flex-col gap-4">
-      <div className="flex justify-center items-center w-80 min-h-80 sm:w-96 sm:min-h-96 rounded-xl shadow-xl">
-        {recordedUrl ? (
-          <video
-            src={recordedUrl}
-            playsInline
-            controls
-            preload="auto"
-            className="w-full aspect-auto rounded-xl"
-          />
-        ) : (
+  if (!recordedUrl) {
+    return (
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-center items-center w-80 min-h-80 sm:w-96 sm:min-h-96 rounded-xl shadow-xl">
           <Image
             src="/video-camera-slash.svg"
             alt="No recording"
             width={60}
             height={60}
           />
-        )}
+        </div>
+
+        <div className="border px-4 py-2 w-full bg-white text-center text-black rounded-xl shadow-xl">
+          파일 없음
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-center items-center w-80 min-h-80 sm:w-96 sm:min-h-96 rounded-xl shadow-xl">
+        <video
+          src={recordedUrl}
+          playsInline
+          controls
+          preload="auto"
+          className="w-full aspect-auto rounded-xl"
+        />
       </div>
 
-      {recordedUrl ? (
+      <div className="w-full h-12">
         <Link
-          href={recordedUrl ? recordedUrl : "#"}
+          href={recordedUrl}
           target="_blank"
           rel="noopener noreferrer"
           download="recording.webm"
-          className="inline-block px-4 py-2 border bg-black text-center text-white rounded-xl shadow-xl"
+          className="inline-block border px-4 py-2 w-full bg-black text-center text-white rounded-xl shadow-xl"
         >
           파일 받기
         </Link>
-      ) : (
-        <button className="px-4 py-2 border text-center text-black rounded-xl shadow-xl">
-          파일 없음
-        </button>
-      )}
+      </div>
     </div>
   );
 }
--- src/component/Recorder.tsx
+++ src/component/Recorder.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { RefObject } from "react";
 
 interface RecorderProps {
@@ -8,6 +9,30 @@ interface RecorderProps {
 }
 
 export function Recorder({ videoRef, isRecording, startRecording, stopRecording }: RecorderProps) {
+  if (!isRecording) {
+    return (
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-center items-center w-80 min-h-80 sm:w-96 sm:min-h-96 rounded-xl shadow-xl">
+          <Image
+            src="/video-camera-slash.svg"
+            alt="No recording"
+            width={60}
+            height={60}
+          />
+        </div>
+
+        <div className="w-full h-12">
+          <button
+            onClick={startRecording}
+            className="border px-4 py-2 w-full bg-black text-white rounded-xl shadow-xl"
+          >
+            녹화 시작
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex justify-center items-center w-80 min-h-[20rem] sm:w-96 sm:min-h-[24rem] rounded-xl shadow-xl">
@@ -16,28 +41,19 @@ export function Recorder({ videoRef, isRecording, startRecording, stopRecording 
             ref={videoRef}
             muted
             playsInline
+            autoPlay
             className="w-full aspect-auto rounded-xl"
           />
         </div>
       </div>
 
       <div className="w-full h-12">
-        {isRecording && (
-          <button
-            onClick={stopRecording}
-            className="border px-4 py-2 w-full bg-white text-black rounded-xl shadow-xl"
-          >
-            녹화 중지
-          </button>
-        )}
-        {!isRecording && (
-          <button
-            onClick={startRecording}
-            className="border px-4 py-2 w-full bg-black text-white rounded-xl shadow-xl"
-          >
-            녹화 시작
-          </button>
-        )}
+        <button
+          onClick={stopRecording}
+          className="border px-4 py-2 w-full bg-white text-black rounded-xl shadow-xl"
+        >
+          녹화 중지
+        </button>
       </div>
     </div>
   );
